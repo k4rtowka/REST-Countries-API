21a9101a6a415de79c9ceab3780a151bf60d4619
k4rtowka <k4rtowkayt@gmail.com>
2023-11-22 00:56:59 +0300

    Add mobile styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,18 +8,19 @@ import light from '../../assets/icon-light.svg';
 const Header = () => {
     const dispatch = useAppDispatch();
     const theme = useAppSelector(state => state.theme.theme)
+    const mode = theme.charAt(0).toUpperCase() + theme.slice(1)
 
 
     return (
         <div className={classes.header}>
-            <h2 style={{fontWeight: "bold", fontSize: "26px"}}>Where in the world?</h2>
-            <span className={classes.rightSide} onClick={() => dispatch(switchTheme())}>
+            <h2 className={classes.title}>Where in the world?</h2>
+            <div className={classes.rightSide} onClick={() => dispatch(switchTheme())}>
                 <img src={theme==='dark'?light:dark} alt={theme}
                      style={{width: "18px", height: "18px"}}
                 >
                 </img>
-                {theme.charAt(0).toUpperCase() + theme.slice(1)} mode
-            </span>
+              <span >{mode} mode</span>
+            </div>
         </div>
     );
 };
